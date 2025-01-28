a72d7019847989cdd3a680d125654ab5618da66c
Cbishop21 <cbishop27@riverdale.edu>
2025-01-28 11:42:57 -0500

    Version 1 no story

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(args[0] + " " + args[1] + " " + args[2] + " " + args[3] + " " + args[4]);
+        System.out.println(args[0] + " " + args[1] + " " + args[2]);
+        System.out.println(args[3] + " " + args[4]);
 
 
     }
